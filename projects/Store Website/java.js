2e52214acc62c6f76f0156e4aac1666768017463
Antonio <antoniogil105@gmail.com>
2020-05-15 21:44:40 -0700

    Completed Mobile Version
    completed all content and layouts for the mobile version

---
+++ projects/Store Website/java.js
@@ -0,0 +1,65 @@
+
+function showall()
+{
+    document.getElementById('remove1').innerHTML = "<img class='img1' src='images/Mexican/paleta-payaso.jpg'> <figcaption>$4</figcaption>";
+    document.getElementById('remove2').innerHTML = "<img class='img2' src='images/American/jolly-rancher.jpg'> <figcaption>$8</figcaption>";
+    document.getElementById('remove3').innerHTML = "<img class='img3' src='images/Australian/cadbury-dairy-milk.jpg'> <figcaption>$6</figcaption>";
+    document.getElementById('remove4').innerHTML = "<img class='img4' src='images/Mexican/pulparindo.jpeg'> <figcaption>$3</figcaption>";
+    document.getElementById('remove5').innerHTML = "<img class='img5' src='images/American/hersheys.jpg'> <figcaption>$5</figcaption>";
+    document.getElementById('remove6').innerHTML = "<img class='img6' src='images/Australian/red-skins.jpg'> <figcaption>$7</figcaption>";
+    document.getElementById('remove7').innerHTML = "<img class='img7' src='images/Mexican/vero-mango.jpg'> <figcaption>$7</figcaption>";
+    document.getElementById('remove8').innerHTML = "<img class='img8' src='images/American/nerds.png'> <figcaption>$5</figcaption>";
+    document.getElementById('remove9').innerHTML = "<img class='img9' src='images/Australian/chicos.jpg'> <figcaption>$8</figcaption>";
+    document.getElementById('remove10').innerHTML = "<img class='img10' src='images/Mexican/pelon.jpg'> <figcaption>$6</figcaption>";
+    document.getElementById('remove11').innerHTML = "<img class='img11' src='images/American/reeses.jpg'> <figcaption>$5</figcaption>";
+    document.getElementById('remove12').innerHTML = "<img class='img12' src='images/Australian/crunchie.jpg'> <figcaption>$4</figcaption>";
+}
+
+function mexican()
+{
+    document.getElementById('remove1').innerHTML = "<img class='img1' src='images/Mexican/paleta-payaso.jpg'> <figcaption>$4</figcaption>";
+    document.getElementById('remove4').innerHTML = "<img class='img4' src='images/Mexican/pulparindo.jpeg'> <figcaption>$3</figcaption>";
+    document.getElementById('remove7').innerHTML = "<img class='img7' src='images/Mexican/vero-mango.jpg'> <figcaption>$7</figcaption>";
+    document.getElementById('remove10').innerHTML = "<img class='img10' src='images/Mexican/pelon.jpg'> <figcaption>$6</figcaption>";
+    document.getElementById('remove2').innerHTML = "";
+    document.getElementById('remove3').innerHTML = "";
+    document.getElementById('remove5').innerHTML = "";
+    document.getElementById('remove6').innerHTML = "";
+    document.getElementById('remove8').innerHTML = "";
+    document.getElementById('remove9').innerHTML = "";
+    document.getElementById('remove11').innerHTML = "";
+    document.getElementById('remove12').innerHTML = "";
+}
+
+function australian()
+{   
+    document.getElementById('remove3').innerHTML = "<img class='img3' src='images/Australian/cadbury-dairy-milk.jpg'> <figcaption>$6</figcaption>";
+    document.getElementById('remove6').innerHTML = "<img class='img6' src='images/Australian/red-skins.jpg'> <figcaption>$7</figcaption>";
+    document.getElementById('remove9').innerHTML = "<img class='img9' src='images/Australian/chicos.jpg'> <figcaption>$8</figcaption>";
+    document.getElementById('remove12').innerHTML = "<img class='img12' src='images/Australian/crunchie.jpg'> <figcaption>$4</figcaption>";
+    document.getElementById('remove1').innerHTML = "";
+    document.getElementById('remove2').innerHTML = "";
+    document.getElementById('remove4').innerHTML = "";
+    document.getElementById('remove5').innerHTML = "";
+    document.getElementById('remove7').innerHTML = "";
+    document.getElementById('remove8').innerHTML = "";
+    document.getElementById('remove10').innerHTML = "";
+    document.getElementById('remove11').innerHTML = "";
+
+}
+
+function american()
+{
+    document.getElementById('remove2').innerHTML = "<img class='img2' src='images/American/jolly-rancher.jpg'> <figcaption>$8</figcaption>";
+    document.getElementById('remove5').innerHTML = "<img class='img5' src='images/American/hersheys.jpg'> <figcaption>$5</figcaption>";
+    document.getElementById('remove8').innerHTML = "<img class='img8' src='images/American/nerds.png'> <figcaption>$5</figcaption>";
+    document.getElementById('remove11').innerHTML = "<img class='img11' src='images/American/reeses.jpg'> <figcaption>$5</figcaption>";
+    document.getElementById('remove1').innerHTML = "";
+    document.getElementById('remove3').innerHTML = "";
+    document.getElementById('remove4').innerHTML = "";
+    document.getElementById('remove6').innerHTML = "";
+    document.getElementById('remove7').innerHTML = "";
+    document.getElementById('remove9').innerHTML = "";
+    document.getElementById('remove10').innerHTML = "";
+    document.getElementById('remove12').innerHTML = "";
+}
